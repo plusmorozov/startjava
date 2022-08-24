c292b040747426df840e02cca7d69cb2b2aa5316
plusmorozov <plusmorozov@yandex.ru>
2022-08-24 14:15:23 +0500

    Implement HW Lesson 1

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,27 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+        int result = 0;
+        char mathOperation = '/';
+        if (mathOperation == '+') {
+            result = num1 + num2;
+        } else if (mathOperation == '-') {
+            result = num1 - num2;
+        } else if (mathOperation == '-') {
+            result = num1 - num2;
+        } else if (mathOperation == '*') {
+            result = num1 * num2;
+        } else if (mathOperation == '/') {
+            result = num1 / num2;
+        } else if (mathOperation == '^') {
+            result = 1;
+            for (int i = 1; i <= num2; i++) {
+                result *= num1;
+            }
+        } else if (mathOperation == '%') {
+            result = num1 % num2;
+        }
+        System.out.println(num1 + " " + mathOperation + " " + num2 + " = " + result);
+    }
+}
---
+++ Lesson_1/MyFirstGame.java
@@ -0,0 +1,16 @@
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int secretNum = 43;
+        int enteredNum = 15;
+        do {
+            if (secretNum > enteredNum) {
+                System.out.println("Число " + enteredNum + " меньше того, что загадал компьютер");
+                enteredNum += secretNum / 2 + 5;
+            } else if (secretNum < enteredNum) {
+                System.out.println("Число " + enteredNum + " больше того, что загадал компьютер");
+                enteredNum -= secretNum / 2 - 5;
+            }
+        } while (secretNum != enteredNum);
+        System.out.println("Вы победили!");
+    }
+}
