c1b2e22b835116bd53051c3cab025df7818bf7c7
plusmorozov <plusmorozov@yandex.ru>
2022-09-22 07:56:19 +0500

    Implement HW Lesson 4. Calculator. v. 4

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -12,8 +12,14 @@ public class CalculatorTest {
             System.out.print("Введите выражение, которое требуется вычислить: ");
             try {
                 System.out.println("Результат: " + Calculator.calculate(scanner.nextLine()));
-            } catch(NumberFormatException | InputMismatchException | IllegalStateException e) {
-                System.out.println("Введены некорректные данные!");
+            } catch(NumberFormatException e) {
+                System.out.println("Числа должны быть целыми");
+                continue;
+            } catch(InputMismatchException e) {
+                System.out.println("Числа должны быть положительными");
+                continue;
+            } catch(IllegalStateException e) {
+                System.out.println("Некорректный знак математической операции");
                 continue;
             }
             do {
