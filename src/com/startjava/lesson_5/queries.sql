4cd04b6cc83dc55e58575434144f6c53b7433420
plusmorozov <plusmorozov@yandex.ru>
2022-10-03 10:42:43 +0500

    Implement HW Lesson 5. v.1

--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -1,42 +1,47 @@
+-- выведите всю таблицу
 SELECT * FROM Jaegers;
 
-SELECT * 
+-- отобразите только не уничтоженных роботов
+SELECT *
   FROM Jaegers
  WHERE status = 'Active';
- 
+
+-- отобразите роботов нескольких серий, например Mark-1 и Mark-6
 SELECT *
   FROM Jaegers
  WHERE mark in (1, 3, 5);
-  
+
+-- отсортируйте таблицу по убыванию по столбцу mark
 SELECT *
   FROM Jaegers
- ORDER BY mark;
- 
+ ORDER BY mark DESC;
+
+-- отобразите самого старого робота
 SELECT *
   FROM Jaegers
  WHERE launch =
-	   (SELECT MIN(launch) 
-	      FROM Jaegers);
-		  
+       (SELECT MIN(launch)
+        FROM Jaegers);
+
+-- отобразите роботов, которые уничтожили больше/меньше всех kaiju
 SELECT *
   FROM Jaegers
- WHERE kaijuKill 
-	IN ((SELECT MIN(kaijuKill) 
-	       FROM Jaegers), 
-	    (SELECT MAX(kaijuKill) 
-	       FROM Jaegers));	 
-		   
+ WHERE kaijuKill
+    IN ((SELECT MIN(kaijuKill)
+           FROM Jaegers),
+        (SELECT MAX(kaijuKill)
+           FROM Jaegers));
+
+-- отобразите средний вес роботов
 SELECT AVG(weight) AS "Средний вес роботов"
   FROM Jaegers;
 
-UPDATE Jaegers 
-   SET kaijuKill = kaijuKill + 1
- WHERE status = 'active';
-  
-UPDATE Jaegers 
+-- увеличьте на единицу количество уничтоженных kaiju у роботов, которые до сих пор не разрушены
+UPDATE Jaegers
    SET kaijuKill = kaijuKill + 1
  WHERE status = 'Active';
- 
+
+-- удалите уничтоженных роботов
  DELETE 
    FROM Jaegers
   WHERE status = 'Destroyed';
