edff4f8479fe8c5727a1d97a0927b99a15559f2e
plusmorozov <plusmorozov@yandex.ru>
2022-10-03 21:25:31 +0500

    Implement HW Lesson 5. v.3

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE Jaegers;
 
 CREATE TABLE Jaegers (
-            id  SERIAL PRIMARY KEY,
-     modelName  TEXT,
-          mark  INTEGER,
-        height  REAL,
-        weight  REAL,
-        status  TEXT,
-        origin  TEXT,
-        launch  INTEGER,
-     kaijuKill  INTEGER
+    id         SERIAL PRIMARY KEY,
+    modelName  TEXT,
+    mark       INTEGER,
+    height     REAL,
+    weight     REAL,
+    status     TEXT,
+    origin     TEXT,
+    launch     INTEGER,
+    kaijuKill  INTEGER
      );
